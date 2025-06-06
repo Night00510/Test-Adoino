02018d668b3f0613029bded78380ca5e64541954
AP_Dev <night00510@gmail.com>
2025-06-06 21:00:19 +0700

    Update aduino_Temperture Sensor.cpp

--- aduino_Temperture Sensor.cpp
+++ aduino_Temperture Sensor.cpp
@@ -1,4 +1,3 @@
-const int SENSOR_PIN = A0; 
 const int RAW_MIN = 20;
 const int RAW_MAX = 358;
 const int TEMP_MIN = -40;
@@ -6,14 +5,14 @@ const int TEMP_MAX = 125;
 
 void setup() {
   // ไม่จำเป็นต้องกำหนด pinMode สำหรับ A0 ใน Arduino เพราะเป็น INPUT โดย default 
-  pinMode(SENSOR_PIN, INPUT); // ใส่มาดีกว่า 
+  pinMode(A0, INPUT); // ใส่มาดีกว่า 
   Serial.begin(9600);
   Serial.println("Arduino Temperature Sensor - Ready"); 
 }
 
 void loop() 
 {
-  int rawSensorValue = analogRead(SENSOR_PIN);// RawData function
+  int rawSensorValue = analogRead(A0);// RawData function
   int temperatureCelsius = map(rawSensorValue, RAW_MIN, RAW_MAX, TEMP_MIN, TEMP_MAX);//MapData to °C function
   //UI function
   Serial.print("Raw: ");
